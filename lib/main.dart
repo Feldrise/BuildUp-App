bd50098d2f6aa9a558fa31b1bc9dd023bcda2da8
Feldrise <admin@feldrise.com>
2020-12-04 23:21:46 +0100

    🎉 Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:buildup/src/pages/autentication/login_page/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Build Up',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFD43744),
+        
+        accentColor: const Color(0xFFD43744),
+
+        cardColor: Colors.white,
+
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 48, color: Color(0xff2c3e50)),
+          headline2: TextStyle(fontSize: 40, color: Color(0xff2c3e50)),
+          headline3: TextStyle(fontSize: 32, color: Color(0xff2c3e50)),
+          bodyText2: TextStyle(fontSize: 16),
+          button: TextStyle(fontSize: 16)
+        )
+      ),
+      home:LoginPage()
+    );
+  }
+}
