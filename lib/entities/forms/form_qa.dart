440fcecd7fbded7ff780cb4aa50ca34d334532ef
DENIS Victor <victordenis01@gmail.com>
2022-02-16 22:51:48 +0100

    🔥 Prepare to start new Flutter project

--- lib/entities/forms/form_qa.dart
+++
@@ -1,8 +0,0 @@
-class FormQA {
-  final String question;
-  final String answer;
-
-  FormQA.fromMap(Map<String, dynamic> map) : 
-    question = map['question'] as String,
-    answer = map['answer'] as String;
-}
