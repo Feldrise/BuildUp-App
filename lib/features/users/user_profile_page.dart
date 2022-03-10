cd051fb458f123533354411dd3cebcaa7076f09f
Feldrise <admin@feldrise.com>
2022-03-10 22:39:32 +0100

    ⚰️ Remove useless code querying for login

--- lib/features/users/user_profile_page.dart
+++ lib/features/users/user_profile_page.dart
@@ -1,6 +1,5 @@
 import 'package:buildup/core/utils/screen_utils.dart';
 import 'package:buildup/core/widgets/bu_status_message.dart';
-import 'package:buildup/features/authentication/authentication_graphql.dart';
 import 'package:buildup/features/project/widgets/project_card.dart';
 import 'package:buildup/features/users/user.dart';
 import 'package:buildup/features/users/users_graphql.dart';
@@ -15,21 +14,16 @@ class UserProfilePage extends StatelessWidget {
     this.appBarTitle,
   }) : super(key: key);
 
-  final String userId;
+  final String? userId;
   final String? appBarTitle;
-
-  QueryOptions<Map<String, dynamic>> _loggedUserOptions() {
-    return QueryOptions<Map<String, dynamic>>(
-      document: gql(qGetLoggedUser)
-    );
-  }
 
   QueryOptions<Map<String, dynamic>> _userOptions() {
     return QueryOptions<Map<String, dynamic>>(
       document: gql(qGetDetailedUser),
       variables: <String, dynamic>{
         "id": userId
-      }
+      },
+      fetchPolicy: FetchPolicy.noCache // TODO: should be fixed
     );
   }
 
@@ -40,26 +34,7 @@ class UserProfilePage extends StatelessWidget {
       appBar: appBarTitle == null ? null : AppBar(
         title: Text(appBarTitle!),
       ),
-      body: Query<Map<String, dynamic>>(
-        options: _loggedUserOptions(),
-        builder: (loggedResult, {fetchMore, refetch}) {
-          if (loggedResult.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          }
-    
-          if (loggedResult.hasException) {
-            return const Align(
-              alignment: Alignment.topLeft,
-              child: BuStatusMessage(
-                message: "Nous n'arrivons pas à charger vos informations. Cette erreur ne devrait pas arriver, n'hésitez pas à nous contacter.",
-              ),
-            );
-          }
-    
-          final User appUser = User.fromJson(loggedResult.data?["user"] as Map<String, dynamic>? ?? <String, dynamic>{});
-
-          // Now we can get the actual user
-          return Query<Map<String, dynamic>>(
+          body: Query<Map<String, dynamic>>(
             options: _userOptions(),
             builder: (userResult, {fetchMore, refetch}) {
               if (userResult.isLoading) {
@@ -79,7 +54,7 @@ class UserProfilePage extends StatelessWidget {
               }
 
               final User user = User.fromJson(userResult.data?["user"] as Map<String, dynamic>? ?? <String, dynamic>{});
-              final bool isLoggedUser = appUser.id == user.id; 
+              final bool isLoggedUser = userId == null;
 
               return SingleChildScrollView(
                 child: Padding(
@@ -106,9 +81,6 @@ class UserProfilePage extends StatelessWidget {
                 ),
               );
             },
-          );
-    
-        },
       ),
     );
   }
